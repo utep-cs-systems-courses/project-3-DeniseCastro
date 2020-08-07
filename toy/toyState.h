e4c7e9e0771395a3f59b83ae4aa1421cb22aecd0
DeniseCastro <castr.denise12@gmail.com>
2020-08-07 05:40:30 -0600

    minor changes to functions in state

--- toy/toyState.h
+++ toy/toyState.h
@@ -1,6 +1,11 @@
 #ifndef toyState_included
 #define toyState_included
 
+void song();
+int melody(int);
+void checkKey(int[]);
+void state_advance();
 void stateMachine();
+char toggle_green();
 
 #endif
